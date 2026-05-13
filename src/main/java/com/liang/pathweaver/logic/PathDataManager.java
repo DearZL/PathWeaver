724f95d3bbe8a949dce4f6d59e2595f1e2f95716
Liang Zhou <dear.zhouliang@qq.com>
2026-05-13 20:51:07 +0800

    update version

--- src/main/java/com/liang/pathweaver/logic/PathDataManager.java
+++ src/main/java/com/liang/pathweaver/logic/PathDataManager.java
@@ -2,9 +2,11 @@ package com.liang.pathweaver.logic;
 
 import com.liang.pathweaver.data.PlayerPathData;
 import com.liang.pathweaver.item.PathWeaverTool;
+import com.liang.pathweaver.registry.ModItems;
 import com.liang.pathweaver.undo.UndoManager;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -18,6 +20,13 @@ public class PathDataManager {
 
     public static PlayerPathData get(UUID uuid) {
         return DATA.computeIfAbsent(uuid, k -> new PlayerPathData());
+    }
+
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
+        if (event.getPlayer().getMainHandItem().is(ModItems.PATH_WEAVER_TOOL.get())) {
+            event.setCanceled(true);
+        }
     }
 
     @SubscribeEvent
--- src/main/java/com/liang/pathweaver/network/ModMessages.java
+++ src/main/java/com/liang/pathweaver/network/ModMessages.java
@@ -2,20 +2,26 @@ package com.liang.pathweaver.network;
 
 import com.liang.pathweaver.PathWeaver;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public class ModMessages {
-    private static final String PROTOCOL = "1";
     public static SimpleChannel CHANNEL;
     private static int id = 0;
+
+    private static String protocolVersion() {
+        return ModList.get().getModContainerById(PathWeaver.MOD_ID)
+                .map(mc -> mc.getModInfo().getVersion().toString())
+                .orElse("0");
+    }
 
     public static void register() {
         CHANNEL = NetworkRegistry.newSimpleChannel(
                 ResourceLocation.fromNamespaceAndPath(PathWeaver.MOD_ID, "main"),
-                () -> PROTOCOL,
-                PROTOCOL::equals,
-                PROTOCOL::equals);
+                ModMessages::protocolVersion,
+                str -> protocolVersion().equals(str),
+                str -> protocolVersion().equals(str));
 
         CHANNEL.registerMessage(id++, C2SMarkPathPointPacket.class,
                 C2SMarkPathPointPacket::encode, C2SMarkPathPointPacket::decode, C2SMarkPathPointPacket::handle);
